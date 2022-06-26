3d017fe3db9edd421c1b14949801710a8c9b2798
joao-vta <joaovta7@gmail.com>
2022-06-26 19:01:44 -0300

    Criei funcionalidades de login e cadastro

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,7 +1,17 @@
 const { isValidObjectId } = require('mongoose');
+const User = require('../models/User');
 const UserModel = require('../models/User')
 
 module.exports.createUser = async (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).send("Some parameters are missing");
+    }
+
+    const userFound = await UserModel.findOne({'username':req.body.username});
+    if (userFound) {
+        return res.status(400).send("Username already exists");
+    }
+
     const newUser = new UserModel({
         username: req.body.username,
         password: req.body.password
@@ -9,4 +19,19 @@ module.exports.createUser = async (req, res) => {
 
     const UserCreated = await newUser.save();
     return res.status(200).json(UserCreated);
+}
+
+module.exports.getUser = async (req, res) => {
+    if (!req.body.username || !req.body.password) 
+        return res.status(400).send("Some parameters are missing");
+
+    const userFound = await UserModel.findOne({'username':req.body.username});
+
+    if (!userFound) 
+        return res.status(400).send("Username does not exist");
+
+    if (userFound.password != req.body.password) 
+        return res.status(400).send("Password doest not match");
+    else 
+        return res.status(200).json(userFound);
 }
--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 // app.use(require("./routes/User")); -> exemplo da monitoria
+app.use(require("./routes/User"));
 const PORT = 5300;
 
 mongoose.connect(process.env.CONN_STR, {useNewUrlParser: true, useUnifiedTopology: true});
